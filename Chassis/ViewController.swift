acc43a9673d29767e858bd709de4130c844d22f0
Daniel Eberle <daniel.eberle@sharecare.com>
2020-09-15 08:11:12 +0200

    Add plain Xcode 12 beta 6 project

---
+++ Chassis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chassis
+//
+//  Created by Daniel Eberle on 15.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
