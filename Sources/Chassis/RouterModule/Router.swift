8d2340cc9ea41f3ebf136415b0e210e49ef9bd5f
Daniel Eberle <eberle@insideguidance.com>
2021-03-02 09:54:25 +0100

    Router changes

--- Sources/Chassis/RouterModule/Router.swift
+++ Sources/Chassis/RouterModule/Router.swift
@@ -17,12 +17,15 @@ public class RoutingEvent: UIEvent {
     let window: UIWindow?
     let route: Route
     let animated: Bool
+    let modal: Bool
 
-    public init(viewController: UIViewController?, window: UIWindow? = nil, route: Route, animated: Bool = true) {
+    public init(viewController: UIViewController?, window: UIWindow? = nil, route: Route, animated: Bool = true, modal: Bool = false) {
         self.viewController = viewController
         self.window = window
         self.route = route
         self.animated = animated
+        self.modal = modal
+        super.init()
     }
 }
 
@@ -34,6 +37,7 @@ public protocol Route {
 public class Router: AppModule {
     
     public let persistenceController: PersistenceController
+    private var rootViewController: UIViewController?
     
     public init(persistenceController: PersistenceController) {
         self.persistenceController = persistenceController
@@ -43,11 +47,43 @@ public class Router: AppModule {
     @IBAction public func route(sender: Any?, event: RoutingEvent) {
 
         if let viewController = event.viewController {
+
             viewController.navigationController?.pushViewController(event.route.viewController(router: self), animated: event.animated)
+        } else if let window = event.window {
+
+            let viewController = event.route.viewController(router: self).wrappendInNavigationController()
+            window.rootViewController = viewController
+            self.rootViewController = window.rootViewController
+        } else if let rootViewController = self.rootViewController {
+
+            let viewController = event.route.viewController(router: self)
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: viewController, action: #selector(UIViewController.dismiss(sender:)))
+            let navigationController = UINavigationController(rootViewController: viewController)
+            rootViewController.present(navigationController, animated: true, completion: nil)
         }
+    }
+}
+
+extension UIViewController {
+
+    public func wrappendInNavigationController() -> UIViewController  {
+
+        var viewController = self
         
-        if let window = event.window {
-            window.rootViewController = event.route.viewController(router: self)
+        let excludedController = [UINavigationController.self, UITabBarController.self, UISplitViewController.self]
+
+        if (excludedController.allSatisfy { !viewController.isKind(of: $0) }) {
+
+            viewController = UINavigationController(rootViewController: viewController)
         }
+        return viewController
+    }
+}
+
+extension UIViewController {
+
+    @objc func dismiss(sender: Any?) {
+
+        self.dismiss(animated: true, completion: nil)
     }
 }
